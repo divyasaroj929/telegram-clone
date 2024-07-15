933b6031b32654f18cbb82cb1df08103411af563
Divya <divyasaroj29@gmail.com>
2024-07-15 13:16:35 +0530

    submit

--- src/components/ChatSection.js
+++ src/components/ChatSection.js
@@ -16,31 +16,39 @@ const ChatSection = ({ chats }) => {
         <React.Fragment key={chat.id}>
           <ListItem button component={Link} to={`/messages/${chat.id}`}>
             <ListItemAvatar>
-              <Badge
-                badgeContent={chat.msg_count}
-                color="primary"
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-              >
-                <Avatar alt={chat.created_by} src={chat.avatar_url} />
-              </Badge>
+              <Avatar alt={chat.created_by} src={chat.avatar_url} />
             </ListItemAvatar>
             <ListItemText
               primary={chat.creator.name}
               secondary={
-                <Typography
-                  sx={{ display: "flex", justifyContent: "space-between" }}
-                  component="div"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  <span>{chat.status}</span>
-                  <span style={{ marginLeft: "auto", fontSize: "0.75rem" }}>
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                    component="div"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    <span>{chat.status}</span>
+                    <Badge
+                      badgeContent={chat.msg_count}
+                      color="primary"
+                      anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                      }}
+                      sx={{ marginLeft: "auto" }}
+                    />
+                  </Typography>
+                  <span
+                    style={{
+                      display: "block",
+                      textAlign: "right",
+                      fontSize: "0.75rem",
+                    }}
+                  >
                     {new Date(chat.created_at).toLocaleDateString()}
                   </span>
-                </Typography>
+                </React.Fragment>
               }
             />
           </ListItem>
